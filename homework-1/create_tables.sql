df6c137ae91995b433a6954961f2647ff6339571
Tegfer <157280106+Tegfer@users.noreply.github.com>
2024-07-28 14:54:30 +0500

    inset codes from creating tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE orders
+(
+	orders_id int PRIMARY KEY,
+	customer_id varchar(6),
+	employee_id int,
+	order_date date,
+	ship_city text
+);
+
+CREATE TABLE employees
+(
+	employees_id int PRIMARY KEY,
+	first_name text,
+	last_name text,
+	title text,
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(6) PRIMARY KEY,
+	company_name text,
+	contact_name text
+);
